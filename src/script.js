cbd26a8557caef97bada35bd901c402d720eb983
Gloriana Angulo Alvarado <g.angulo@b7-09.local>
2019-02-06 10:40:38 -0600

    Agrego funcionalidad de insertar los datos a la tabla

--- src/script.js
+++ src/script.js
@@ -1,7 +1,25 @@
-/**
- * Descripción del programa
- * ...
- */
+const btnSave = document.getElementById('save');
+const name1 = document.getElementById('name');
+const type1 = document.getElementById('type');
+const date1 = document.getElementById('date');
+const amount1 = document.getElementById('amount');
+// const tablita = document.getElementById('tablita');
+const lista = [];
 
-// Aquí va el código del programa.
-console.log('Hola Mundo!');
+function save() {
+  const factura = document.getElementById('factura');
+  let fila = '';
+  const nuevaFila = {
+    name: name1.value,
+    type: type1.value,
+    date: date1.value,
+    amount: amount1.value,
+  };
+  lista.push(nuevaFila);
+  for (let i = 0; i < lista.length; i += 1) {
+    fila += `<tr><td> ${lista[i].name} </td><td>  ${lista[i].type} </td><td> ${lista[i].date} </td><td> ${lista[i].amount} </td></tr>`;
+  }
+  factura.innerHTML = fila;
+}
+btnSave.addEventListener('click', save);
+console.log(type1[0].value)
